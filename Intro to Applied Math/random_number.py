02dfc2b1fb793706b32261fab8d419260ab44a02
Caitlyn Jones <143729818+caitlynj10@users.noreply.github.com>
2026-01-21 18:50:49 -0500

    Delete Intro to Applied Math directory

--- Intro to Applied Math/random_number.py
+++
@@ -1,9 +0,0 @@
-# Draw a random number from 1 to N, where N is provided as an
-# argument to the function.
-
-import numpy as np
-
-N = 10
-
-n = np.random.randint(N)+1
-print("Random number between 1 and 10:",n)
